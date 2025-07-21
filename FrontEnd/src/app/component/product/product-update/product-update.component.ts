a747469afd27933aed6cb2ae685347bfb3b78939
PhGomesBr <ph.gmontanha1@gmail.com>
2025-07-21 17:45:42 -0300

    atualizando tela de update pro

--- FrontEnd/src/app/component/product/product-update/product-update.component.ts
+++ FrontEnd/src/app/component/product/product-update/product-update.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FornecedorService } from '../../fornecedor/fornecedor.service';
+import { Fornecedor } from '../../fornecedor/fornecedor.model';
 
 @Component({
   selector: 'app-product-update',
@@ -10,16 +12,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductUpdateComponent {
   product!: Product;
+  fornecedores: Fornecedor[]= []
 
   constructor(private productService: ProductService,
     private router: Router,
+    private fornecedorService: FornecedorService,
     private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('proId')
     this.productService.readById(id!).subscribe((product: Product) =>{
       this.product = product
-    })
+    });
+    this.fornecedorService.readFornecedores().subscribe((data: Fornecedor[]) => {
+      this.fornecedores = data;
+    });
   }
 
   updateProduct(): void {
